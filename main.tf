92afa4345c782bf8302f62333d4f590644b11038
ned1313 <ned.bellavance@gmail.com>
2024-04-29 09:42:52 -0400

    working

---
+++ main.tf
@@ -0,0 +1,46 @@
+# Provider: AzureRM
+provider "azurerm" {
+  features {}
+
+}
+
+# Randmon ID for resource group
+resource "random_integer" "main" {
+  min = 10000
+  max = 99999
+}
+
+locals {
+  resource_group_name = "${var.prefix}-${var.environment}-${random_integer.main.result}"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = local.resource_group_name
+  location = var.location
+  tags = {
+    environment = var.environment
+  }
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = var.vnet_address_space
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+# Subnets
+resource "azurerm_subnet" "main" {
+  for_each = var.subnets
+
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [each.value]
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  type        = string
+  description = "Environment value to use for tagging resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Location value to use for Azure resources"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix value to use for Azure resources"
+}
+
+variable "vnet_address_space" {
+  type        = list(string)
+  description = "Address space for the virtual network"
+}
+
+variable "subnets" {
+  type        = map(string)
+  description = "Subnets to create in the virtual network"
+
+}
