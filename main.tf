a15786a71ed28941a25fa6e3c81c7c3ef14cd0b2
ned1313 <ned.bellavance@gmail.com>
2024-05-03 13:25:50 -0400

    fix formatting

--- main.tf
+++ main.tf
@@ -15,18 +15,18 @@ locals {
   resource_group_name = "${var.prefix}-${var.environment}-${random_integer.main.result}"
   environments = {
     development = {
-        vnet_address_space = ["10.64.0.0/16"]
-        subnets = {
-            web = "10.64.0.0/24"
-            app = "10.64.0.0/24"
-        }
+      vnet_address_space = ["10.64.0.0/16"]
+      subnets = {
+        web = "10.64.0.0/24"
+        app = "10.64.0.0/24"
+      }
     }
     staging = {
-        vnet_address_space = ["10.65.0.0/16"]
-        subnets = {
-            web = "10.65.0.0/24"
-            app = "10.65.0.0/24"
-        }
+      vnet_address_space = ["10.65.0.0/16"]
+      subnets = {
+        web = "10.65.0.0/24"
+        app = "10.65.0.0/24"
+      }
     }
   }
 }
